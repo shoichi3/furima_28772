478acf48697e93529575ed876822fd55cfd5a856
shoichi3 <dece12shou@gmail.com>
2020-08-29 11:34:56 +0900

    itemsコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :detail, :category_id, :status_id, :burden_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
 end
