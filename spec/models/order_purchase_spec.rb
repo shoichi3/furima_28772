5e4244e54dbeb8b1a0c4f68df1cad6ee8185b13e
shoichi3 <dece12shou@gmail.com>
2020-09-02 18:22:11 +0900

    テストコードの設定

---
+++ spec/models/order_purchase_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe OrderPurchase, type: :model do
+  describe '購入情報の保存' do
+    before do
+      @order_purchase = FactoryBot.build(:order_purchase)
+    end
+
+    it '全ての値が正しく入力されている場合保存できること' do
+      expect(@order_purchase).to be_valid
+    end
+
+    it 'tokenが空の場合保存できないこと' do
+      @order_purchase.token = nil
+      @order_purchase.valid?
+      expect(@order_purchase.errors.full_messages).to include("Token can't be blank")
+    end
+    
+    it 'postal_codeが空の場合保存できないこと' do
+      @order_purchase.postal_code = nil
+      @order_purchase.valid?
+      expect(@order_purchase.errors.full_messages).to include("Postal code can't be blank")
+    end
+
+    it 'postal_codeでハイフンがない場合保存できないこと' do
+      @order_purchase.postal_code = "1234567"
+      @order_purchase.valid?
+      expect(@order_purchase.errors.full_messages).to include("Postal code Input correctly")
+    end
+
+    it 'prefecture_idが1の場合保存できないこと' do
+      @order_purchase.prefecture_id = "1"
+      @order_purchase.valid?
+      expect(@order_purchase.errors.full_messages).to include("Prefecture Select")
+    end
+
+    it 'cityが空の場合保存できないこと' do
+      @order_purchase.city = nil
+      @order_purchase.valid?
+      expect(@order_purchase.errors.full_messages).to include("City can't be blank")
+    end
+
+    it 'addressが空の場合保存できないこと' do
+      @order_purchase.address = nil
+      @order_purchase.valid?
+      expect(@order_purchase.errors.full_messages).to include("Address can't be blank")
+    end
+
+    it 'phone_numberが空の場合保存できないこと' do
+      @order_purchase.phone_number = nil
+      @order_purchase.valid?
+      expect(@order_purchase.errors.full_messages).to include("Phone number can't be blank")
+    end
+  end
+end
