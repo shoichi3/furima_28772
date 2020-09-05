745dbc69845fc2b544977b06f49d97fe957d0b89
shoichi3 <dece12shou@gmail.com>
2020-09-05 17:41:00 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima_28772'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'shoichi3/furima_28772.git'
+set :repo_url,  'git@github.com:shoichi3/furima_28772.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
