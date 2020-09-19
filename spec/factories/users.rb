29bd6460c474bc4a7b7abb8347a7093d877e05ac
shoichi3 <dece12shou@gmail.com>
2020-09-19 11:28:32 +0900

    キーの変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     password                 { Faker::Internet.password(min_length: 6) }
     password_confirmation    { password }
     birth_date               { '2000-01-13' }
-    first_name               { '山田' }
-    last_name                { '太郎' }
-    first_name_kana          { 'ヤマダ' }
-    last_name_kana           { 'タロウ' }
+    last_name                { '山田' }
+    first_name               { '太郎' }
+    last_name_kana           { 'ヤマダ' }
+    first_name_kana          { 'タロウ' }
   end
 end
